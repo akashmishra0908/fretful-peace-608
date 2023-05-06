e16611904b2da391185cf979a419a89d08220115
akashmishra0908 <112822976+akashmishra0908@users.noreply.github.com>
2023-05-07 00:21:56 +0530

    project

---
+++ myprojectreact/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  let url=`https://localhost:akash
+  /posts`
+  console.log(url);
+  return (
+    <div className="App">
+     <h1>
+     </h1>
+    </div>
+  );
+}
+
+export default App;
